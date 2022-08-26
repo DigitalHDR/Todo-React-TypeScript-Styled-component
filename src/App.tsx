8c9da996ada0a7328eec3363bd354b9f0c9ab003
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-08-26 10:33:34 -0300

    types genericos

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
+import { useState } from 'react'
 import * as C from './App.styles'
+import { Item } from './types/item'
+import { ListItem } from './components/ListItem'
 
 const App = () => {
-  return <C.Container>
-    <C.Area>
-      <C.Header>Lista de Tarefas</C.Header>
-    </C.Area>
-  </C.Container>
+  const [list, setList] = useState<Item[]>([
+    {
+      id: 1,
+      name: 'comprar o pão na padaria',
+      done: false,
+    },
+    {
+      id: 2,
+      name: 'comprar o bolo na padaria',
+      done: false,
+    },
+  ])
+
+  return (
+    <C.Container>
+      <C.Area>
+        <C.Header>Lista de Tarefas</C.Header>
+
+        {/* área de adicionar nova tarefa */}
+
+        {list.map((item, index) => (
+          <ListItem />
+        ))}
+      </C.Area>
+    </C.Container>
+  )
 }
 
 export default App
+
+
+29:59
---
+++ src/components/ListItem/index.tsx
@@ -0,0 +1,7 @@
+import * as C from './styles'
+
+export const ListItem = () => {
+  return (
+    <C.Container>hello world</C.Container>
+  )
+}
